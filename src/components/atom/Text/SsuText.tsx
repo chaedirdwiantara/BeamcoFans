b469358bd4aa06079682ab1dd481116ad37e66b3
chaedir dwiantara <chaedirdwiantara@chaedirs-MacBook-Pro.local>
2022-10-12 20:18:26 +0700

    add body text component

--- src/components/atom/Text/SsuText.tsx
+++ src/components/atom/Text/SsuText.tsx
@@ -1,4 +1,5 @@
 
 import { default as Headline } from './Headline';
+import { default as Body } from './Body';
 /** === EXPORT COMPONENT === */
-export const SsuText = { Headline};
+export const SsuText = { Headline, Body};
--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import { SsuText } from '../components/atom/Text/SsuText';
+import { color } from '../theme';
 
 export const HomeScreen: React.FC = () => {
   return (
@@ -8,7 +9,7 @@ export const HomeScreen: React.FC = () => {
       <Text>Home Screen</Text>
       <SsuText.Headline.Large>Update your password</SsuText.Headline.Large>
       <SsuText.Headline.Default>Select your expectation as a fans</SsuText.Headline.Default>
-      <SsuText.Headline.Small>SSU Warehouse Project</SsuText.Headline.Small>
+      <SsuText.Headline.Small color={color.Success[500]}>SSU Warehouse Project</SsuText.Headline.Small>
       <SsuText.Headline.Tiny>Top Musician</SsuText.Headline.Tiny>
     </View>
   );
