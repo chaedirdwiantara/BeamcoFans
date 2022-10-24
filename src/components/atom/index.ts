55d785a2fe4cf3e68e6123eb7b1e14ad3695cc19
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-10-24 17:54:55 +0700

    Update import components

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -1,6 +1,20 @@
-import Gap from "./Gap/Gap";
-import Avatar from "./Avatar/Avatar"
-import { SsuInput } from "./InputText/SsuInput";
-import { SsuText } from "./Text/SsuText";
+import Gap from './Gap/Gap';
+import {Avatar, AvatarUsername} from './Avatar/Avatar';
+import {Button} from './Button/Button';
+import {ChipMoney} from './ChipMoney/ChipMoney';
+import DescriptionBoarding from './DescriptionBoarding/DescriptionBoarding';
+import {MenuText} from './MenuText/MenuText';
+import {SsuInput} from './InputText/SsuInput';
+import {SsuText} from './Text/SsuText';
 
-export {Gap, Avatar, SsuInput, SsuText}
+export {
+  Gap,
+  Avatar,
+  AvatarUsername,
+  Button,
+  ChipMoney,
+  DescriptionBoarding,
+  MenuText,
+  SsuInput,
+  SsuText,
+};
