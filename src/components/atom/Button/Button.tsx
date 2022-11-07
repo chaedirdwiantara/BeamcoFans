f0d99d472b5f410bb8102508b2b3251784cd400a
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-11-07 20:13:45 +0700

    Add new props

--- src/components/atom/Button/Button.tsx
+++ src/components/atom/Button/Button.tsx
@@ -18,15 +18,23 @@ interface ButtonProps {
   borderColor?: string;
   containerStyles?: ViewStyle;
   textStyles?: TextStyle;
-  onPress: () => void;
+  disabled?: boolean;
+  onPress?: () => void;
 }
 
 export const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
-  const {type, label, borderColor, containerStyles, textStyles, onPress} =
-    props;
+  const {
+    type,
+    label,
+    borderColor,
+    containerStyles,
+    textStyles,
+    disabled,
+    onPress,
+  } = props;
 
   const withBorder = type === 'border' && {
-    borderWidth: ms(2),
+    borderWidth: ms(1),
     borderColor: borderColor ? borderColor : Color.Pink.linear,
     backgroundColor: 'transparent',
   };
@@ -34,6 +42,7 @@ export const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
   return (
     <TouchableOpacity
       style={[styles.root, withBorder, containerStyles]}
+      disabled={disabled}
       onPress={onPress}>
       <Text style={[styles.labelStyle, textStyles]}>{label}</Text>
     </TouchableOpacity>
--- src/components/atom/Indicator/Indicator.tsx
+++ src/components/atom/Indicator/Indicator.tsx
@@ -4,15 +4,21 @@ import {ms, mvs} from 'react-native-size-matters';
 import Color from '../../../theme/Color';
 interface IndicatorProps {
   activeIndex?: boolean;
+  type?: string;
 }
 
-export const Indicator: React.FC<IndicatorProps> = ({activeIndex}) => {
+export const Indicator: React.FC<IndicatorProps> = ({activeIndex, type}) => {
+  const bgActvButton =
+    type === 'preference' ? Color.Dark[100] : Color.Success[400];
+  const bgNonActvButton =
+    type === 'preference' ? Color.Dark[300] : Color.Success[400];
+
   return (
     <View>
       {activeIndex ? (
-        <View style={styles.active} />
+        <View style={[styles.active, {backgroundColor: bgActvButton}]} />
       ) : (
-        <View style={styles.nonActive} />
+        <View style={[styles.nonActive, {backgroundColor: bgNonActvButton}]} />
       )}
     </View>
   );
@@ -23,12 +29,10 @@ const styles = StyleSheet.create({
     width: ms(32),
     height: mvs(6),
     borderRadius: 4,
-    backgroundColor: Color.Success[400],
   },
   nonActive: {
     width: ms(6),
     height: ms(6),
     borderRadius: 4,
-    backgroundColor: Color.Success[700],
   },
 });
