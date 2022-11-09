fd6ebc6c19f924b866daef4dfe19f554363d26f9
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-11-09 10:18:57 +0700

    Update index ts molecule

--- src/components/molecule/index.ts
+++ src/components/molecule/index.ts
@@ -1,35 +1,39 @@
 import {AvatarUsername} from './AvatarUsername/AvatarUsername';
+import {Dropdown} from './DropDown';
 import {FollowArtistCard} from './FollowArtistCard/FollowArtistCard';
 import {FollowingCard} from './FollowingCard/FollowingCard';
 import {ImageSlider} from './ImageSlider';
 import {ListCard} from './ListCard';
 import {LoginDescription} from './LoginDescription/LoginDescription';
 import {ModalConfirm} from './Modal/ModalConfirm';
+import {NoMoreFeeds} from './NoMoreFeeds/NoMoreFeeds';
 import {NotificationCard} from './NotificationCard/NotificationCard';
 import {PreferenceContent} from './PreferenceContent/PreferenceContent';
 import {ProfileHeader} from './ProfileHeader/ProfileHeader';
-import {TopNavigation} from  './TopNavigation';
-import { Dropdown } from './DropDown';
-import { TabFilter } from './TabFilter';
-import TermAndConditions from './Tnc';
+import {ReferralContent} from './ReferralContent/ReferralContent';
 import SsuOTPInput from './OtpInput';
 import SsuOTPTimer from './OtpTimer';
+import {TabFilter} from './TabFilter';
+import TermAndConditions from './Tnc';
+import {TopNavigation} from './TopNavigation';
 
 export {
   AvatarUsername,
+  Dropdown,
   FollowArtistCard,
   FollowingCard,
   ImageSlider,
   ListCard,
   LoginDescription,
   ModalConfirm,
+  NoMoreFeeds,
   NotificationCard,
   PreferenceContent,
   ProfileHeader,
-  TopNavigation,
-  Dropdown,
+  ReferralContent,
+  SsuOTPInput,
+  SsuOTPTimer,
   TabFilter,
   TermAndConditions,
-  SsuOTPInput,
-  SsuOTPTimer
+  TopNavigation,
 };
