d0450eaf1154600951157a5b7a9d4f310470da41
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-11-11 10:42:31 +0700

    Add dummy data

---
+++ src/data/home.tsx
@@ -0,0 +1,45 @@
+export interface DataSliderType {
+  id?: number;
+  title?: string;
+  uri?: string;
+  subtitle?: string;
+}
+
+export const dataSlider: DataSliderType[] = [
+  {
+    id: 0,
+    uri: 'https://img.lemde.fr/2022/06/09/0/411/5083/3389/1440/960/60/0/6870439_1654768589970-hole-in-your-soul-abba-voyage-photo-by-johan-persson-1.jpg',
+    title: 'ABBA Concert',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+  },
+  {
+    id: 1,
+    uri: 'https://imageio.forbes.com/specials-images/imageserve/614af094b90e7b76aac40223/0x0.jpg?format=jpg&width=1200',
+    title: 'Global Citizen Festival',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+  },
+  {
+    id: 2,
+    uri: 'https://assets.pikiran-rakyat.com/crop/0x54:1080x720/x/photo/2022/05/19/849530363.jpg',
+    title: 'BTS Concert',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+  },
+  {
+    id: 3,
+    uri: 'https://stietribhakti.ac.id/wp-content/uploads/2018/08/austin-neill-247047-unsplash.jpg',
+    title: 'Concert for Charity',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+  },
+  {
+    id: 4,
+    uri: 'https://www.billboard.com/wp-content/uploads/2021/08/concert-crowd-billboard-1548-1629382874.jpg?w=942&h=623&crop=1',
+    title: 'Canceled Concert Due to COVID',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+  },
+  {
+    id: 5,
+    uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Muse_in_Sydney.jpg/1200px-Muse_in_Sydney.jpg',
+    title: 'Concert',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+  },
+];
--- src/data/onboard.tsx
+++ src/data/onboard.tsx
@@ -1,29 +1,25 @@
 import {ImageSourcePropType} from 'react-native';
 
 export interface DataOnboardType {
-  id: number;
-  uri: ImageSourcePropType;
   title: string;
-  subtitle: string;
+  uri?: ImageSourcePropType;
+  subtitle?: string;
 }
 
 export const dataOnboard: DataOnboardType[] = [
   {
-    id: 0,
     uri: require('../assets/background/onboard-1.png'),
     title: "Let's the fans",
     subtitle:
       'Lorem ipsum dolor sit amet, consectetur adispicing elit. Vestibulum porta ipsum Lorem ipsum dolor sit amet.',
   },
   {
-    id: 1,
     uri: require('../assets/background/onboard-2.png'),
     title: 'with their musician',
     subtitle:
       'Lorem ipsum dolor sit amet, consectetur adispicing elit. Vestibulum porta ipsum Lorem ipsum dolor sit amet.',
   },
   {
-    id: 2,
     uri: require('../assets/background/onboard-3.png'),
     title: "Let's Let's Let's",
     subtitle:
