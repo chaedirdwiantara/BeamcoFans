e608816f2922fdeb15c898475159d26a196544d1
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-11-15 20:34:21 +0700

    Add new icons

--- src/assets/icon/index.tsx
+++ src/assets/icon/index.tsx
@@ -1,4 +1,7 @@
+import AddIcon from './Add.icon';
+import ArrowLeftIcon from './ArrowLeft.icon';
 import ArrowRightIcon from './ArrowRight.icon';
+import CameraIcon from './Camera';
 import CheckBoxIcon from './CheckBox.icon';
 import CheckCircleIcon from './CheckCircle.icon';
 import ChevronDownIcon from './ChevronDown.icon';
@@ -6,17 +9,22 @@ import CloseIcon from './Close.icon';
 import CoinIcon from './Coin.icon';
 import CollectionIcon from './Collection.icon';
 import CommentIcon from './Comment.icon';
+import CrownIcon from './Crown.icon';
 import EditIcon from './Edit.icon';
+import EmailIcon from './Email.icon';
 import ErrorIcon from './Error.icon';
 import ExportIcon from './Export.icon';
 import EyeCloseIcon from './EyeClose.icon';
 import EyeOpenIcon from './EyeOpen.icon';
 import FeedIcon from './Feed.icon';
+import FullNameIcon from './FullName.icon';
 import GalleryEditIcon from './GalleryEdit.icon';
 import GiftIcon from './Gift.icon';
 import HeartIcon from './Heart.icon';
 import HomeIcon from './Home.icon';
 import KeyIcon from './Key.icon';
+import LockIcon from './Lock.icon.';
+import LogOutIcon from './Logout.icon';
 import LoveIcon from './Love.icon';
 import MedalIcon from './Medal.icon';
 import MusicPlaylistIcon from './MusicPlaylist.icon';
@@ -24,18 +32,20 @@ import MusicSquareIcon from './MusicSquare.icon';
 import MusicSquareAddIcon from './MusicSquareAdd.icon';
 import NotificationIcon from './Notification.icon';
 import ProcessingIcon from './Processing.icon';
+import SaveIcon from './Save.icon';
 import SearchIcon from './Search.icon';
 import SettingIcon from './Setting.icon';
 import ShareIcon from './Share.icon';
 import ThreeDotsIcon from './ThreeDots.icon';
 import TickCircleIcon from './TickCircle.icon';
 import UserIcon from './User.icon';
-import FullNameIcon from './FullName.icon';
-import EmailIcon from './Email.icon';
-import LockIcon from './Lock.icon.';
+import UserProfileIcon from './UserProfile.icon';
 
 export {
+  AddIcon,
+  ArrowLeftIcon,
   ArrowRightIcon,
+  CameraIcon,
   CheckBoxIcon,
   CheckCircleIcon,
   ChevronDownIcon,
@@ -43,17 +53,22 @@ export {
   CoinIcon,
   CollectionIcon,
   CommentIcon,
+  CrownIcon,
   EditIcon,
+  EmailIcon,
   ErrorIcon,
   ExportIcon,
   EyeCloseIcon,
   EyeOpenIcon,
   FeedIcon,
+  FullNameIcon,
   GalleryEditIcon,
   GiftIcon,
   HeartIcon,
   HomeIcon,
   KeyIcon,
+  LockIcon,
+  LogOutIcon,
   LoveIcon,
   MedalIcon,
   MusicPlaylistIcon,
@@ -61,13 +76,12 @@ export {
   MusicSquareAddIcon,
   NotificationIcon,
   ProcessingIcon,
+  SaveIcon,
   SearchIcon,
   SettingIcon,
   ShareIcon,
   ThreeDotsIcon,
   TickCircleIcon,
   UserIcon,
-  FullNameIcon,
-  EmailIcon,
-  LockIcon,
+  UserProfileIcon,
 };
