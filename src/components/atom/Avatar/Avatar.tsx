b08e6563f6ec1572d5975d98407c751a8f66f040
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-11-16 00:44:05 +0700

    Add new type avatar

--- src/components/atom/Avatar/Avatar.tsx
+++ src/components/atom/Avatar/Avatar.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import {Image, StyleSheet, View, TouchableOpacity} from 'react-native';
 import {ms} from 'react-native-size-matters';
 
 interface AvatarProps {
   imgUri: string;
   size?: number;
   username?: string;
+  showIcon?: boolean;
+  onPress?: () => void;
+  icon?: React.ReactNode;
 }
 
 export const Avatar: React.FC<AvatarProps> = (props: AvatarProps) => {
-  const {imgUri, size = ms(32)} = props;
+  const {imgUri, size = ms(32), showIcon, icon, onPress} = props;
+
+  const iconComp = () => {
+    return (
+      <TouchableOpacity style={[styles.icon, {width: size}]} onPress={onPress}>
+        {icon}
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <Image
-      source={{uri: imgUri}}
-      style={[styles.root, {width: size}]}
-      testID={'ssu-avatar'}
-    />
+    <View>
+      {showIcon && iconComp()}
+      <Image
+        source={{uri: imgUri}}
+        style={[styles.root, {width: size}]}
+        testID={'ssu-avatar'}
+      />
+    </View>
   );
 };
 
@@ -24,5 +39,15 @@ const styles = StyleSheet.create({
     height: undefined,
     aspectRatio: 1 / 1,
     borderRadius: 200,
+  },
+  icon: {
+    zIndex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
