ebcc5e5604d75303e0cde87c62693db8f3ed967f
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-11-16 00:46:46 +0700

    update index component

--- src/components/molecule/index.tsx
+++ src/components/molecule/index.tsx
@@ -1,41 +1,49 @@
 import {AvatarUsername} from './AvatarUsername/AvatarUsername';
 import {Carousel} from './Carousel/Carousel';
+import {CreateNewCard} from './CreateNewCard/CreateNewCard';
 import {Dropdown} from './DropDown';
+import {EditProfile} from './ProfileContent/EditProfile';
 import {FollowArtistCard} from './FollowArtistCard/FollowArtistCard';
-import {FollowingCard} from './FollowingCard/FollowingCard';
 import {ImageSlider} from './ImageSlider/ImageSlider';
 import {ListCard} from './ListCard';
 import {LoginDescription} from './LoginDescription/LoginDescription';
 import {ModalConfirm} from './Modal/ModalConfirm';
 import {NoMoreFeeds} from './NoMoreFeeds/NoMoreFeeds';
 import {NotificationCard} from './NotificationCard/NotificationCard';
-import {ProfileHeader} from './ProfileHeader/ProfileHeader';
+import {ProfileContent} from './ProfileContent/ProfileContent';
+import {ProfileHeader} from './ProfileContent/Header';
 import {ReferralContent} from './ReferralContent/ReferralContent';
+import {SettingContent} from './SettingContent/SettingContent';
 import {SignInGuestContent} from './SignInGuestContent/SignInGuestContent';
 import SsuOTPInput from './OtpInput';
 import SsuOTPTimer from './OtpTimer';
 import {TabFilter} from './TabFilter';
 import TermAndConditions from './Tnc';
 import {TopNavigation} from './TopNavigation';
+import {UserInfoCard} from './UserInfoCard/UserInfoCard';
 
 export {
   AvatarUsername,
   Carousel,
+  CreateNewCard,
   Dropdown,
+  EditProfile,
   FollowArtistCard,
-  FollowingCard,
   ImageSlider,
   ListCard,
   LoginDescription,
   ModalConfirm,
   NoMoreFeeds,
   NotificationCard,
+  ProfileContent,
   ProfileHeader,
   ReferralContent,
+  SettingContent,
   SignInGuestContent,
   SsuOTPInput,
   SsuOTPTimer,
   TabFilter,
   TermAndConditions,
   TopNavigation,
+  UserInfoCard,
 };
