0e0bc2298093305a230b60e0a34af241c92a0b2c
kriptonhaz <kriptonhaz@gmail.com>
2022-11-15 19:11:31 +0700

    fix style dynamic signup screen

--- src/screen/Signup.tsx
+++ src/screen/Signup.tsx
@@ -1,13 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Dimensions,
-  Image,
-} from 'react-native';
-import {useForm, Controller, SubmitHandler} from 'react-hook-form';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {useForm, SubmitHandler} from 'react-hook-form';
 import {Button, Gap, SsuDivider, SsuInput} from '../components/atom';
 import {yupResolver} from '@hookform/resolvers/yup';
 import * as yup from 'yup';
@@ -16,12 +9,11 @@ import {RegistrationType} from '../interface/profile.interface';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParams} from '../App';
-import SsuSheet from '../components/atom/SsuSheet';
 import {EmailIcon, FullNameIcon, LockIcon, UserIcon} from '../assets/icon';
 import {color, font} from '../theme';
 import {Dropdown, TermAndConditions} from '../components';
 import {countryData} from '../data/dropdown';
-import {normalize} from '../utils';
+import {heightPercentage, normalize, widthPercentage} from '../utils';
 import {AppleLogo, FacebookLogo, GoogleLogo} from '../assets/logo';
 import {ms, mvs} from 'react-native-size-matters';
 
@@ -55,6 +47,13 @@ export const SignupScreen: React.FC = () => {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParams>>();
   const {onRegisterUser, isLoading, isError, authResult} = useAuthHook();
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeat, setRepeat] = useState('');
+  const [error, setError] = useState(false);
+  const [focusInput, setFocusInput] = useState<'email' | 'phone' | null>(null);
 
   const {
     control,
@@ -92,19 +91,22 @@ export const SignupScreen: React.FC = () => {
   const handleOnPressButton = () => {
     navigation.navigate('Preference');
   };
+
   const handleOnPressBack = () => {
-    navigation.goBack();
+    if (focusInput === null) {
+      navigation.goBack();
+    } else {
+      handleFocusInput(null);
+    }
   };
+
   const handleOnPressSignIn = () => {
     navigation.navigate('Login');
   };
 
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeat, setRepeat] = useState('');
-  const [error, setError] = useState(false);
+  const handleFocusInput = (focus: 'email' | 'phone' | null) => {
+    setFocusInput(focus);
+  };
 
   const resultData = (dataResult: any) => {
     console.log(dataResult, 'dataResult Select Country');
@@ -115,145 +117,127 @@ export const SignupScreen: React.FC = () => {
     setTerm(!term);
   };
 
-  const children = () => {
-    return (
-      <>
-        <Text style={styles.titleStyle}>Create Account</Text>
-        <Gap height={20} />
-        <SsuInput.InputText
-          value={fullName}
-          onChangeText={(newText: any) => setFullName(newText)}
-          placeholder={'Full Name'}
-          leftIcon={
-            <FullNameIcon
-              stroke={color.Dark[50]}
-              style={{marginLeft: ms(-2), marginRight: ms(-3)}}
-            />
-          }
-        />
-        <Gap height={8} />
-        <SsuInput.InputText
-          value={email}
-          onChangeText={(newText: any) => setEmail(newText)}
-          placeholder={'Email'}
-          leftIcon={<EmailIcon stroke={color.Dark[50]} />}
-        />
-        <Gap height={8} />
-        <SsuInput.InputText
-          value={userName}
-          onChangeText={(newText: any) => setUserName(newText)}
-          placeholder={'Username'}
-          leftIcon={
-            <UserIcon
-              stroke={color.Dark[50]}
-              style={{marginLeft: ms(-1), marginRight: ms(-4)}}
-            />
-          }
-        />
-        <Gap height={8} />
-        <SsuInput.InputText
-          value={password}
-          onChangeText={(newText: any) => setPassword(newText)}
-          placeholder={'Password'}
-          leftIcon={<LockIcon stroke={color.Dark[50]} />}
-          password
-        />
-        <Gap height={8} />
-        <SsuInput.InputText
-          value={repeat}
-          onChangeText={(newText: any) => setRepeat(newText)}
-          placeholder={'Repeat Password'}
-          isError={error}
-          errorMsg={'Incorrect username or password'}
-          leftIcon={<LockIcon stroke={color.Dark[50]} />}
-          password
-        />
-        <Gap height={12} />
-        <SsuDivider text={'or'} />
-        <Gap height={8} />
-        <Dropdown.Country countryData={countryData} numberTyped={resultData} />
-        <Gap height={14} />
-        <TermAndConditions handleOnPress={handleOnPressTnc} active={term} />
-        <Gap height={20} />
-        <Button
-          label="Submit"
-          textStyles={{fontSize: normalize(14)}}
-          containerStyles={{width: '100%'}}
-          onPress={handleOnPressButton}
-        />
-        <Gap height={4} />
-        <Button
-          type="border"
-          label="Back"
-          borderColor="transparent"
-          textStyles={{fontSize: normalize(14), color: color.Pink.linear}}
-          containerStyles={{width: '100%'}}
-          onPress={handleOnPressBack}
-        />
-        <Gap height={8} />
-        <SsuDivider text={'or signup with'} />
-        <Gap height={14} />
-        <View
-          style={{
-            flexDirection: 'row',
-            width: '100%',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <GoogleLogo />
-          <Gap width={24} />
-          <FacebookLogo />
-          <Gap width={24} />
-          <AppleLogo />
-        </View>
-        <Gap height={24} />
-        <Text style={styles.forgotPassStyle}>
-          Already have an Account?{' '}
-          <Text
-            onPress={() => handleOnPressSignIn()}
-            style={{
-              fontFamily: font.InterRegular,
-              fontWeight: '700',
-              fontSize: normalize(12),
-              lineHeight: mvs(16),
-            }}>
-            Sign In
-          </Text>
-        </Text>
-      </>
-    );
-  };
-
   return (
     <View style={styles.root}>
-      {/* <Text>Signup screen</Text>
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-        }}
-        render={({field: {onChange, value}}) => (
-          <View style={styles.container}>
-            <SsuInput.InputText
-              value={value}
-              onChangeText={onChange}
-              errorMsg={errors.username?.message}
-              isError={errors.username ? true : false}
-            />
-          </View>
-        )}
-        name="username"
+      <Text style={styles.titleStyle}>Create Account</Text>
+      <Gap height={32} />
+      <SsuInput.InputText
+        value={fullName}
+        onChangeText={(newText: any) => setFullName(newText)}
+        placeholder={'Full Name'}
+        leftIcon={
+          <FullNameIcon
+            stroke={color.Dark[50]}
+            style={{marginLeft: ms(-2), marginRight: ms(-3)}}
+          />
+        }
       />
-      <TouchableOpacity
-        disabled={isLoading}
-        onPress={handleSubmit(handleRegisterUser)}>
-        <Text>Signup</Text>
-      </TouchableOpacity> */}
-      <Image
-        source={require('../assets/background/signin-guest.png')}
-        style={styles.image}
+      {(focusInput === 'email' || focusInput === null) && (
+        <>
+          <Gap height={8} />
+          <SsuInput.InputText
+            value={email}
+            onChangeText={(newText: any) => setEmail(newText)}
+            placeholder={'Email'}
+            leftIcon={<EmailIcon stroke={color.Dark[50]} />}
+            onFocus={() => handleFocusInput('email')}
+          />
+        </>
+      )}
+      <Gap height={8} />
+      <SsuInput.InputText
+        value={userName}
+        onChangeText={(newText: any) => setUserName(newText)}
+        placeholder={'Username'}
+        leftIcon={
+          <UserIcon
+            stroke={color.Dark[50]}
+            style={{marginLeft: ms(-1), marginRight: ms(-4)}}
+          />
+        }
       />
-      <SsuSheet children={children()} />
+      <Gap height={8} />
+      <SsuInput.InputText
+        value={password}
+        onChangeText={(newText: any) => setPassword(newText)}
+        placeholder={'Password'}
+        leftIcon={<LockIcon stroke={color.Dark[50]} />}
+        password
+      />
+      <Gap height={8} />
+      <SsuInput.InputText
+        value={repeat}
+        onChangeText={(newText: any) => setRepeat(newText)}
+        placeholder={'Repeat Password'}
+        isError={error}
+        errorMsg={'Incorrect username or password'}
+        leftIcon={<LockIcon stroke={color.Dark[50]} />}
+        password
+      />
+      {focusInput === null && (
+        <>
+          <Gap height={12} />
+          <SsuDivider text={'or'} />
+        </>
+      )}
+      {(focusInput === 'phone' || focusInput === null) && (
+        <>
+          <Gap height={8} />
+          <Dropdown.Country
+            countryData={countryData}
+            numberTyped={resultData}
+            onFocus={() => handleFocusInput('phone')}
+          />
+        </>
+      )}
+      <Gap height={14} />
+      <TermAndConditions handleOnPress={handleOnPressTnc} active={term} />
+      <Gap height={20} />
+      <Button
+        label="Submit"
+        textStyles={{fontSize: normalize(14)}}
+        containerStyles={{width: '100%'}}
+        onPress={handleOnPressButton}
+      />
+      <Gap height={4} />
+      <Button
+        type="border"
+        label="Back"
+        borderColor="transparent"
+        textStyles={{fontSize: normalize(14), color: color.Pink.linear}}
+        containerStyles={{width: '100%'}}
+        onPress={handleOnPressBack}
+      />
+      <Gap height={8} />
+      <SsuDivider text={'or signup with'} />
+      <Gap height={14} />
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <GoogleLogo />
+        <Gap width={24} />
+        <FacebookLogo />
+        <Gap width={24} />
+        <AppleLogo />
+      </View>
+      <Gap height={24} />
+      <Text style={styles.forgotPassStyle}>
+        Already have an Account?{' '}
+        <Text
+          onPress={() => handleOnPressSignIn()}
+          style={{
+            fontFamily: font.InterRegular,
+            fontWeight: '700',
+            fontSize: normalize(12),
+            lineHeight: mvs(16),
+          }}>
+          Sign In
+        </Text>
+      </Text>
     </View>
   );
 };
@@ -263,6 +247,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    height: '100%',
+    backgroundColor: color.Dark[800],
+    paddingHorizontal: widthPercentage(48),
+    paddingTop: heightPercentage(32),
+    paddingBottom: heightPercentage(24),
   },
   container: {
     width: '100%',
