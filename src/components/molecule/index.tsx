cc81b419620b11781ec65fb7bf300ec0a495aa71
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-11-22 07:53:34 +0700

    Add import new components in index

--- src/components/molecule/index.tsx
+++ src/components/molecule/index.tsx
@@ -1,9 +1,16 @@
+import {AccountContent} from './SettingContent/AccountContent';
 import {AvatarUsername} from './AvatarUsername/AvatarUsername';
 import {Carousel} from './Carousel/Carousel';
+import {ChangeEmailContent} from './SettingContent/ChangeEmailContent';
+import {ChangePasswordContent} from './SettingContent/ChangePasswordContent';
 import {CreateNewCard} from './CreateNewCard/CreateNewCard';
+import {CreateNewPlaylistContent} from './PlaylistContent/CreateNewPlaylistContent';
 import {Dropdown} from './DropDown';
 import {EditProfile} from './ProfileContent/EditProfile';
+import {EmailContent} from './SettingContent/EmailContent';
+import {EmptyState} from './EmptyState/EmptyState';
 import {FollowArtistCard} from './FollowArtistCard/FollowArtistCard';
+import {FollowingList} from './ProfileContent/components/FollowingList';
 import IconNotif from './IconNotif';
 import {ImageSlider} from './ImageSlider/ImageSlider';
 import {ListCard} from './ListCard';
@@ -11,8 +18,9 @@ import {LoginDescription} from './LoginDescription/LoginDescription';
 import {ModalConfirm} from './Modal/ModalConfirm';
 import {NoMoreFeeds} from './NoMoreFeeds/NoMoreFeeds';
 import {NotificationCard} from './NotificationCard/NotificationCard';
+import {PlaylistContent} from './PlaylistContent/PlaylistContent';
 import {ProfileContent} from './ProfileContent/ProfileContent';
-import {ProfileHeader} from './ProfileContent/Header';
+import {ProfileHeader} from './ProfileContent/components/Header';
 import {ReferralContent} from './ReferralContent/ReferralContent';
 import {SettingContent} from './SettingContent/SettingContent';
 import {SignInGuestContent} from './SignInGuestContent/SignInGuestContent';
@@ -24,12 +32,19 @@ import {TopNavigation} from './TopNavigation';
 import {UserInfoCard} from './UserInfoCard/UserInfoCard';
 
 export {
+  AccountContent,
   AvatarUsername,
   Carousel,
+  ChangeEmailContent,
+  ChangePasswordContent,
   CreateNewCard,
+  CreateNewPlaylistContent,
   Dropdown,
   EditProfile,
+  EmailContent,
+  EmptyState,
   FollowArtistCard,
+  FollowingList,
   IconNotif,
   ImageSlider,
   ListCard,
@@ -37,6 +52,7 @@ export {
   ModalConfirm,
   NoMoreFeeds,
   NotificationCard,
+  PlaylistContent,
   ProfileContent,
   ProfileHeader,
   ReferralContent,
