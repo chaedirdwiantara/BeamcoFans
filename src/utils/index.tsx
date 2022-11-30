c87a2eaf6698f6e6de1b2392962ab8904b41ae10
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-11-30 20:12:12 +0700

    SM-11: Add initial name function for avatar

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,4 +2,5 @@ export * from './currency-format';
 export * from './checkEmptyProperties';
 export * from './dimensionFormat';
 export * from './formatter';
+export * from './initialname';
 export * from './regexNumber';
