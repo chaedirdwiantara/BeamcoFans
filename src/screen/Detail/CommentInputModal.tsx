96955846e9fd9bbee4e8e571f9c44553b29435e7
chaedir dwiantara <chaedirdwiantara@chaedirs-MacBook-Pro.local>
2022-12-02 06:30:54 +0700

    fix ui modal comment input

--- src/screen/Detail/CommentInputModal.tsx
+++ src/screen/Detail/CommentInputModal.tsx
@@ -1,14 +1,18 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {FC, useState} from 'react';
 import Modal from 'react-native-modal';
-import {heightPercentage, heightResponsive, widthResponsive} from '../../utils';
-import {
-  heightPercentageToDP,
-  widthPercentageToDP,
-} from 'react-native-responsive-screen';
-import {Avatar, Gap, SsuInput} from '../../components';
+import {heightPercentage, normalize, widthResponsive} from '../../utils';
+import {Avatar, SsuInput} from '../../components';
 import {color, font} from '../../theme';
 import {ms} from 'react-native-size-matters';
+import {CloseIcon} from '../../assets/icon';
 
 interface ModalImageProps {
   toggleModal: () => void;
@@ -25,33 +29,48 @@ const CommentInputModal: FC<ModalImageProps> = (props: ModalImageProps) => {
       isVisible={modalVisible}
       backdropOpacity={0}
       onBackdropPress={toggleModal}
-      onBackButtonPress={toggleModal}>
+      onBackButtonPress={toggleModal}
+      style={styles.modalStyle}>
       <View style={styles.container}>
+        <View style={styles.headerComment}>
+          <Text style={styles.textHeader}>
+            Replied to<Text style={{color: color.Pink[100]}}> @lorem</Text>
+          </Text>
+          <TouchableOpacity style={styles.closeButton} onPress={toggleModal}>
+            <CloseIcon
+              height={ms(8)}
+              width={ms(8)}
+              stroke={color.Neutral[10]}
+            />
+          </TouchableOpacity>
+        </View>
         <View style={styles.inputContainer}>
-          <Avatar
-            imgUri="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxwjjgqL0vLByyI1sXSGF3Q08PXEmPTUbL6w&usqp=CAU"
-            size={42}
-          />
-          {/* <Gap width={12} /> */}
-          <SsuInput.InputText
-            value={state}
-            onChangeText={(newText: any) => setState(newText)}
-            placeholder={'Reply as your name'}
-            containerStyles={{
-              width: widthResponsive(209),
-              backgroundColor: 'transparent',
-              paddingLeft: 0,
-              flexShrink: 1,
-              maxWidth: widthResponsive(209),
-            }}
-            multiline={true}
-          />
-          <Gap width={24} />
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Avatar
+              imgUri="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxwjjgqL0vLByyI1sXSGF3Q08PXEmPTUbL6w&usqp=CAU"
+              size={32}
+            />
+            <SsuInput.InputText
+              value={state}
+              onChangeText={(newText: any) => setState(newText)}
+              placeholder={'Reply as <your name>...'}
+              containerStyles={{
+                width: widthResponsive(209),
+                backgroundColor: 'transparent',
+                paddingLeft: 0,
+              }}
+              multiline={true}
+            />
+          </View>
           <TouchableOpacity style={styles.buttonStyle}>
             <Text style={styles.buttonText}>Reply</Text>
           </TouchableOpacity>
         </View>
       </View>
+      <KeyboardAvoidingView
+        behavior={
+          Platform.OS === 'ios' ? 'padding' : 'height'
+        }></KeyboardAvoidingView>
     </Modal>
   );
 };
@@ -59,24 +78,44 @@ const CommentInputModal: FC<ModalImageProps> = (props: ModalImageProps) => {
 export default CommentInputModal;
 
 const styles = StyleSheet.create({
+  modalStyle: {
+    marginHorizontal: 0,
+    marginVertical: 0,
+    justifyContent: 'flex-end',
+  },
   container: {
-    marginTop: heightPercentageToDP('90%'),
-    height: heightPercentage(88),
     width: '100%',
     backgroundColor: color.Dark[800],
+  },
+  headerComment: {
+    flexDirection: 'row',
+    width: '100%',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: color.Dark[500],
+    paddingHorizontal: widthResponsive(24),
+    paddingVertical: heightPercentage(6),
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textHeader: {
+    color: color.Dark[50],
+    fontFamily: font.InterRegular,
+    fontWeight: '500',
+    fontSize: normalize(10),
   },
   inputContainer: {
     flexDirection: 'row',
     width: '100%',
     alignItems: 'center',
-    // backgroundColor: 'grey',
-    // height: heightPercentage(64),
+    justifyContent: 'space-between',
+    paddingHorizontal: widthResponsive(24),
+    paddingVertical: heightPercentage(16),
   },
   buttonStyle: {
     backgroundColor: color.Dark[50],
     borderRadius: 4,
     paddingVertical: ms(6),
-    // paddingHorizontal: ms(16),
     width: widthResponsive(62),
     alignItems: 'center',
     justifyContent: 'center',
@@ -86,5 +125,9 @@ const styles = StyleSheet.create({
     fontFamily: font.InterRegular,
     fontWeight: '600',
     fontSize: 11,
+  },
+  closeButton: {
+    width: widthResponsive(24),
+    alignItems: 'center',
   },
 });
