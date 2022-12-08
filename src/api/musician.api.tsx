de81afaa424bd77406b387ddb9a748175f7b5f49
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-12-08 11:08:36 +0700

    SM-58: Add API follow unfollow

--- src/api/musician.api.tsx
+++ src/api/musician.api.tsx
@@ -1,10 +1,38 @@
-import {ListMusicianResponseType} from '../interface/musician.interface';
 import SsuAPI from './base';
+import {
+  FollowMusicianPropsType,
+  FollowMusicianResponseType,
+  ListMusicianResponseType,
+} from '../interface/musician.interface';
 
 export const listMusician = async (): Promise<ListMusicianResponseType> => {
   const {data} = await SsuAPI().request<ListMusicianResponseType>({
     url: '/musicians',
     method: 'GET',
+  });
+
+  return data;
+};
+
+export const followMusician = async (
+  props?: FollowMusicianPropsType,
+): Promise<FollowMusicianResponseType> => {
+  const {data} = await SsuAPI().request<FollowMusicianResponseType>({
+    url: '/musicians/follow',
+    method: 'POST',
+    data: props,
+  });
+
+  return data;
+};
+
+export const unfollowMusician = async (
+  props?: FollowMusicianPropsType,
+): Promise<FollowMusicianResponseType> => {
+  const {data} = await SsuAPI().request<FollowMusicianResponseType>({
+    url: '/musicians/unfollow',
+    method: 'POST',
+    data: props,
   });
 
   return data;
