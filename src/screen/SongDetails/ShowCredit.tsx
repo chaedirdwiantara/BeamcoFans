255fab598bc421e4df4cedb783aaa456856da329
kriptonhaz <kriptonhaz@gmail.com>
2022-12-20 10:15:31 +0700

    refactor navigation param

--- src/screen/SongDetails/ShowCredit.tsx
+++ src/screen/SongDetails/ShowCredit.tsx
@@ -3,7 +3,7 @@ import {View, StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import Color from '../../theme/Color';
-import {RootStackParams} from '../../App';
+import {RootStackParams} from '../../navigations';
 import {ShowCreditContent} from '../../components';
 
 export const ShowCreditScreen: React.FC = () => {
