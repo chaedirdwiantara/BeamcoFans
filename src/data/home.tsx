5b8af8e360d2c71d9c5c602028a920caeb4707de
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-12-21 20:30:20 +0700

    SM-122: Wiring banner

--- src/data/home.tsx
+++ src/data/home.tsx
@@ -1,39 +1,44 @@
-export interface DataSliderType {
-  id?: number;
-  title?: string;
-  uri?: string;
-  subtitle?: string;
-}
+import {BannerList} from '../interface/banner.interface';
 
-export const dataSlider: DataSliderType[] = [
-  {
-    id: 0,
-    uri: 'https://img.lemde.fr/2022/06/09/0/411/5083/3389/1440/960/60/0/6870439_1654768589970-hole-in-your-soul-abba-voyage-photo-by-johan-persson-1.jpg',
-    title: 'ABBA Concert',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
-  },
+export const dataSlider: BannerList[] = [
   {
     id: 1,
-    uri: 'https://imageio.forbes.com/specials-images/imageserve/614af094b90e7b76aac40223/0x0.jpg?format=jpg&width=1200',
-    title: 'Global Citizen Festival',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+    imageUrl:
+      'https://img.lemde.fr/2022/06/09/0/411/5083/3389/1440/960/60/0/6870439_1654768589970-hole-in-your-soul-abba-voyage-photo-by-johan-persson-1.jpg',
+    title: 'ABBA Concert',
+    description: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+    linkUrl: 'https://sunnysideup.io',
   },
   {
     id: 2,
-    uri: 'https://assets.pikiran-rakyat.com/crop/0x54:1080x720/x/photo/2022/05/19/849530363.jpg',
-    title: 'BTS Concert',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+    imageUrl:
+      'https://imageio.forbes.com/specials-images/imageserve/614af094b90e7b76aac40223/0x0.jpg?format=jpg&width=1200',
+    title: 'Global Citizen Festival',
+    description: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+    linkUrl: 'https://sunnysideup.io',
   },
   {
     id: 3,
-    uri: 'https://stietribhakti.ac.id/wp-content/uploads/2018/08/austin-neill-247047-unsplash.jpg',
-    title: 'Concert for Charity',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+    imageUrl:
+      'https://assets.pikiran-rakyat.com/crop/0x54:1080x720/x/photo/2022/05/19/849530363.jpg',
+    title: 'BTS Concert',
+    description: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+    linkUrl: 'https://sunnysideup.io',
   },
   {
     id: 4,
-    uri: 'https://www.billboard.com/wp-content/uploads/2021/08/concert-crowd-billboard-1548-1629382874.jpg?w=942&h=623&crop=1',
+    imageUrl:
+      'https://stietribhakti.ac.id/wp-content/uploads/2018/08/austin-neill-247047-unsplash.jpg',
+    title: 'Concert for Charity',
+    description: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+    linkUrl: 'https://sunnysideup.io',
+  },
+  {
+    id: 5,
+    imageUrl:
+      'https://www.billboard.com/wp-content/uploads/2021/08/concert-crowd-billboard-1548-1629382874.jpg?w=942&h=623&crop=1',
     title: 'Canceled Concert Due to COVID',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+    description: 'Lorem ipsum dolor sit amet, consectetur adispicing elit',
+    linkUrl: 'https://sunnysideup.io',
   },
 ];
