53207db18b658a0b47320a2e37cd3a6598233797
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2022-12-22 09:18:52 +0700

    SM-171: Handle guest user

--- src/screen/Feed.tsx
+++ src/screen/Feed.tsx
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import {widthPercentageToDP} from 'react-native-responsive-screen';
-import {TabFilter, TopNavigation} from '../components';
-import {dropDownDataCategory, dropDownDataSort} from '../data/dropdown';
-import {PostlistData, PostlistDataExclusive} from '../data/postlist';
+
 import {color} from '../theme';
+import {storage} from '../hooks/use-storage.hook';
+import PostListPublic from './ListCard/PostListPublic';
 import {heightPercentage, widthResponsive} from '../utils';
 import PostListExclusive from './ListCard/PostListExclusive';
-import PostListPublic from './ListCard/PostListPublic';
+import {GuestContent, TabFilter, TopNavigation} from '../components';
+import {PostlistData, PostlistDataExclusive} from '../data/postlist';
+import {dropDownDataCategory, dropDownDataSort} from '../data/dropdown';
 
 export const FeedScreen: React.FC = () => {
   const [selectedIndex, setSelectedIndex] = useState(-0);
@@ -15,54 +17,61 @@ export const FeedScreen: React.FC = () => {
     {filterName: 'Public'},
     {filterName: 'Exclusive'},
   ]);
+  const isLogin = storage.getString('profile');
 
   const filterData = (item: any, index: any) => {
     setSelectedIndex(index);
   };
   return (
     <View style={styles.root}>
-      <TopNavigation.Type2
-        title="FEED"
-        maxLengthTitle={20}
-        itemStrokeColor={'white'}
-      />
-      <View
-        style={{
-          marginTop: heightPercentage(8),
-          paddingHorizontal: widthResponsive(24),
-          width: '100%',
-          height: '100%',
-        }}>
-        <TabFilter.Type1
-          filterData={filter}
-          onPress={filterData}
-          selectedIndex={selectedIndex}
-          flatlistContainerStyle={{
-            justifyContent: 'space-between',
-          }}
-          TouchableStyle={{width: widthPercentageToDP(45)}}
-        />
-        {filter[selectedIndex].filterName === 'Public' ? (
-          <PostListPublic
-            dataRightDropdown={dropDownDataCategory}
-            dataLeftDropdown={dropDownDataSort}
-            data={PostlistData}
+      {isLogin ? (
+        <View>
+          <TopNavigation.Type2
+            title="FEED"
+            maxLengthTitle={20}
+            itemStrokeColor={'white'}
           />
-        ) : (
-          <PostListExclusive
-            dataRightDropdown={dropDownDataCategory}
-            dataLeftDropdown={dropDownDataSort}
-            data={PostlistDataExclusive}
-          />
-        )}
-      </View>
+          <View
+            style={{
+              marginTop: heightPercentage(8),
+              paddingHorizontal: widthResponsive(24),
+              width: '100%',
+              height: '100%',
+            }}>
+            <TabFilter.Type1
+              filterData={filter}
+              onPress={filterData}
+              selectedIndex={selectedIndex}
+              flatlistContainerStyle={{
+                justifyContent: 'space-between',
+              }}
+              TouchableStyle={{width: widthPercentageToDP(45)}}
+            />
+            {filter[selectedIndex].filterName === 'Public' ? (
+              <PostListPublic
+                dataRightDropdown={dropDownDataCategory}
+                dataLeftDropdown={dropDownDataSort}
+                data={PostlistData}
+              />
+            ) : (
+              <PostListExclusive
+                dataRightDropdown={dropDownDataCategory}
+                dataLeftDropdown={dropDownDataSort}
+                data={PostlistDataExclusive}
+              />
+            )}
+          </View>
+        </View>
+      ) : (
+        <GuestContent />
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   root: {
-    // flex: 1,
+    flex: 1,
     backgroundColor: color.Dark[800],
   },
 });
