c87a55794d4db19ee2029d02fffdf5887d094ec2
nekaartajaya <nekaartajaya@gmail.com>
2022-12-27 20:14:52 +0800

    feat: update event screen

--- src/screen/Event.tsx
+++ src/screen/Event.tsx
@@ -1,21 +1,63 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import Color from '../theme/Color';
-import {RootStackParams} from '../navigations';
+import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {storage} from '../hooks/use-storage.hook';
+import {GuestContent, TabFilter, TopNavigation} from '../components';
+import {Text} from 'react-native-svg';
+import {heightPercentage, widthPercentage, widthResponsive} from '../utils';
+import {widthPercentageToDP} from 'react-native-responsive-screen';
+import {color} from '../theme';
+import CartIcon from '../assets/icon/CartIcon';
+import BoxStore from '../assets/icon/BoxStore';
 
 export const EventScreen: React.FC = () => {
-  const navigation =
-    useNavigation<NativeStackNavigationProp<RootStackParams>>();
+  const [selectedIndex, setSelectedIndex] = useState(-0);
+  const [filter] = useState([{filterName: 'Concert'}, {filterName: 'Merch'}]);
+  const isLogin = storage.getString('profile');
 
-  const goToScreenReferral = () => {
-    navigation.navigate('Referral');
+  const filterData = (item: any, index: any) => {
+    setSelectedIndex(index);
   };
 
   return (
     <View style={styles.root}>
-      <Text>Event Screen</Text>
+      {isLogin ? (
+        <View>
+          <TopNavigation.Type4
+            title="Store"
+            maxLengthTitle={20}
+            itemStrokeColor={'white'}
+            rightIcon={<CartIcon />}
+            rightIconAction={() => null}
+            leftIcon={<BoxStore />}
+            leftIconAction={() => null}
+            containerStyles={{paddingHorizontal: widthPercentage(20)}}
+          />
+          <View
+            style={{
+              marginTop: heightPercentage(8),
+              paddingHorizontal: widthResponsive(24),
+              width: '100%',
+              height: '100%',
+            }}>
+            <TabFilter.Type1
+              filterData={filter}
+              onPress={filterData}
+              selectedIndex={selectedIndex}
+              flatlistContainerStyle={{
+                justifyContent: 'space-between',
+              }}
+              TouchableStyle={{width: widthPercentageToDP(45)}}
+            />
+            {filter[selectedIndex].filterName === 'Concert' ? (
+              <Text>Concert</Text>
+            ) : (
+              <Text>Merch</Text>
+            )}
+          </View>
+        </View>
+      ) : (
+        <GuestContent />
+      )}
     </View>
   );
 };
@@ -23,8 +65,6 @@ export const EventScreen: React.FC = () => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Color.Dark[800],
+    backgroundColor: color.Dark[800],
   },
 });
