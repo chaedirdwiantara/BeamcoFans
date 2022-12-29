de99825a9a063517888d78494bbad1c121610a1f
nekaartajaya <nekaartajaya@gmail.com>
2022-12-29 20:09:59 +0800

    fix: signup keyboard avoid view

--- src/screen/Signup.tsx
+++ src/screen/Signup.tsx
@@ -155,219 +155,220 @@ export const SignupScreen: React.FC = () => {
   return (
     <View style={styles.root}>
       <SsuStatusBar type="black" />
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.scrollView}>
-        <View>
-          <Text style={styles.titleStyle}>Sign Up</Text>
-          <Gap height={24} />
-          <View
-            style={{
-              flexDirection: 'row',
-              width: '100%',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <GoogleLogo />
-            <Gap width={24} />
-            <FacebookLogo />
-            {Platform.OS === 'ios' ? <Gap width={24} /> : null}
-            {Platform.OS === 'ios' ? <AppleLogo /> : null}
-          </View>
-          <Gap height={20} />
-          <SsuDivider text={'or'} />
-          <Gap height={20} />
-          <View style={styles.wrapperLoginType}>
-            <Text
-              style={
-                watch('registrationType') === 'email'
-                  ? styles.loginTypeActive
-                  : styles.loginTypeInactive
-              }
-              onPress={() => handleChangeLoginType('email')}>
-              Email
-            </Text>
-            <View style={styles.verticalSeparatorLoginType} />
-            <Text
-              style={
-                watch('registrationType') === 'phone'
-                  ? styles.loginTypeActive
-                  : styles.loginTypeInactive
-              }
-              onPress={() => handleChangeLoginType('phone')}>
-              Phone Number
-            </Text>
-          </View>
-          <Gap height={16} />
-          {watch('registrationType') === 'email' ? (
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={heightResponsive(10)}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.scrollView}>
+          <View>
+            <Text style={styles.titleStyle}>Sign Up</Text>
+            <Gap height={24} />
+            <View
+              style={{
+                flexDirection: 'row',
+                width: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <GoogleLogo />
+              <Gap width={24} />
+              <FacebookLogo />
+              {Platform.OS === 'ios' ? <Gap width={24} /> : null}
+              {Platform.OS === 'ios' ? <AppleLogo /> : null}
+            </View>
+            <Gap height={20} />
+            <SsuDivider text={'or'} />
+            <Gap height={20} />
+            <View style={styles.wrapperLoginType}>
+              <Text
+                style={
+                  watch('registrationType') === 'email'
+                    ? styles.loginTypeActive
+                    : styles.loginTypeInactive
+                }
+                onPress={() => handleChangeLoginType('email')}>
+                Email
+              </Text>
+              <View style={styles.verticalSeparatorLoginType} />
+              <Text
+                style={
+                  watch('registrationType') === 'phone'
+                    ? styles.loginTypeActive
+                    : styles.loginTypeInactive
+                }
+                onPress={() => handleChangeLoginType('phone')}>
+                Phone Number
+              </Text>
+            </View>
+            <Gap height={16} />
+            {watch('registrationType') === 'email' ? (
+              <Controller
+                name="email"
+                control={control}
+                render={({field: {onChange, value}}) => (
+                  <SsuInput.InputText
+                    value={value}
+                    onChangeText={onChange}
+                    placeholder={'Email'}
+                    leftIcon={<EmailIcon stroke={color.Dark[50]} />}
+                    onFocus={() => handleFocusInput('email')}
+                    onBlur={() => {
+                      handleFocusInput(null);
+                    }}
+                    isError={errors?.email ? true : false}
+                    errorMsg={errors?.email?.message}
+                    isFocus={focusInput === 'email'}
+                  />
+                )}
+              />
+            ) : (
+              <View>
+                <Controller
+                  name="phone"
+                  control={control}
+                  render={({field: {onChange, value}}) => (
+                    <Dropdown.Country
+                      value={value}
+                      onChangeText={onChange}
+                      countryData={countryData}
+                      numberTyped={resultData}
+                      onFocus={() => handleFocusInput('phone')}
+                      isError={errors?.phone ? true : false}
+                      errorMsg={errors?.phone?.message}
+                      isFocus={focusInput === 'phone'}
+                    />
+                  )}
+                />
+              </View>
+            )}
+            <Gap height={8} />
             <Controller
-              name="email"
+              name="fullname"
               control={control}
               render={({field: {onChange, value}}) => (
                 <SsuInput.InputText
                   value={value}
                   onChangeText={onChange}
-                  placeholder={'Email'}
-                  leftIcon={<EmailIcon stroke={color.Dark[50]} />}
-                  onFocus={() => handleFocusInput('email')}
+                  placeholder={'Full Name'}
+                  leftIcon={<FullNameIcon stroke={color.Dark[50]} />}
+                  onFocus={() => {
+                    handleFocusInput('fullname');
+                  }}
                   onBlur={() => {
                     handleFocusInput(null);
                   }}
-                  isError={errors?.email ? true : false}
-                  errorMsg={errors?.email?.message}
-                  isFocus={focusInput === 'email'}
+                  isError={errors?.fullname ? true : false}
+                  errorMsg={errors?.fullname?.message}
+                  isFocus={focusInput === 'fullname'}
                 />
               )}
             />
-          ) : (
-            <View>
-              <Controller
-                name="phone"
-                control={control}
-                render={({field: {onChange, value}}) => (
-                  <Dropdown.Country
-                    value={value}
-                    onChangeText={onChange}
-                    countryData={countryData}
-                    numberTyped={resultData}
-                    onFocus={() => handleFocusInput('phone')}
-                    isError={errors?.phone ? true : false}
-                    errorMsg={errors?.phone?.message}
-                    isFocus={focusInput === 'phone'}
-                  />
-                )}
-              />
-            </View>
-          )}
-          <Gap height={8} />
-          <Controller
-            name="fullname"
-            control={control}
-            render={({field: {onChange, value}}) => (
-              <SsuInput.InputText
-                value={value}
-                onChangeText={onChange}
-                placeholder={'Full Name'}
-                leftIcon={<FullNameIcon stroke={color.Dark[50]} />}
-                onFocus={() => {
-                  handleFocusInput('fullname');
-                }}
-                onBlur={() => {
-                  handleFocusInput(null);
-                }}
-                isError={errors?.fullname ? true : false}
-                errorMsg={errors?.fullname?.message}
-                isFocus={focusInput === 'fullname'}
-              />
-            )}
-          />
-          <Gap height={8} />
-          <Controller
-            name="password"
-            control={control}
-            render={({field: {onChange, value}}) => (
-              <SsuInput.InputText
-                value={value}
-                onChangeText={onChange}
-                placeholder={'Password'}
-                leftIcon={<LockIcon stroke={color.Dark[50]} />}
-                password
-                onFocus={() => {
-                  handleFocusInput('password');
-                }}
-                onBlur={() => {
-                  handleFocusInput(null);
-                }}
-                isError={errors?.password ? true : false}
-                errorMsg={errors?.password?.message}
-                isFocus={focusInput === 'password'}
-              />
-            )}
-          />
-          <Gap height={8} />
-          <Controller
-            name="confirmPassword"
-            control={control}
-            render={({field: {onChange, value}}) => (
-              <SsuInput.InputText
-                value={value}
-                onChangeText={onChange}
-                placeholder={'Repeat'}
-                leftIcon={<LockIcon stroke={color.Dark[50]} />}
-                password
-                onFocus={() => {
-                  handleFocusInput('confirmPassword');
-                }}
-                onBlur={() => {
-                  handleFocusInput(null);
-                }}
-                isError={errors?.confirmPassword ? true : false}
-                errorMsg={errors?.confirmPassword?.message}
-                isFocus={focusInput === 'confirmPassword'}
-              />
-            )}
-          />
-          <Gap height={14} />
-          <Controller
-            name="termsCondition"
-            control={control}
-            render={({field: {onChange, value}}) => (
-              <TermAndConditions
-                handleOnPress={() => onChange(!value)}
-                active={value}
-                errorMsg={errors?.termsCondition?.message}
-                onTncPress={() =>
-                  handleWebview(
-                    'Terms Conditions',
-                    'https://sunnysideup.io/marketplace/tos',
-                  )
-                }
-                onPrivacyPress={() => {
-                  handleWebview(
-                    'Privacy Policy',
-                    'https://sunnysideup.io/marketplace/privacy-policy',
-                  );
-                }}
-              />
-            )}
-          />
-          <Gap height={20} />
-          <Button
-            label="Submit"
-            textStyles={{fontSize: mvs(14)}}
-            containerStyles={{width: '100%'}}
-            onPress={handleSubmit(handleRegisterUser)}
-          />
-          <Gap height={4} />
-          <Button
-            label="Back"
-            type="border"
-            borderColor="transparent"
-            textStyles={{fontSize: mvs(14), color: color.Pink.linear}}
-            containerStyles={{width: '100%'}}
-            onPress={handleOnPressBack}
-          />
-        </View>
-        <View>
-          <Text style={styles.forgotPassStyle}>
-            Already have an Account?{' '}
-            <Text
-              onPress={() => handleOnPressSignIn()}
-              style={{
-                fontFamily: font.InterRegular,
-                fontWeight: '700',
-                fontSize: mvs(12),
-              }}>
-              Sign In
+            <Gap height={8} />
+            <Controller
+              name="password"
+              control={control}
+              render={({field: {onChange, value}}) => (
+                <SsuInput.InputText
+                  value={value}
+                  onChangeText={onChange}
+                  placeholder={'Password'}
+                  leftIcon={<LockIcon stroke={color.Dark[50]} />}
+                  password
+                  onFocus={() => {
+                    handleFocusInput('password');
+                  }}
+                  onBlur={() => {
+                    handleFocusInput(null);
+                  }}
+                  isError={errors?.password ? true : false}
+                  errorMsg={errors?.password?.message}
+                  isFocus={focusInput === 'password'}
+                />
+              )}
+            />
+            <Gap height={8} />
+            <Controller
+              name="confirmPassword"
+              control={control}
+              render={({field: {onChange, value}}) => (
+                <SsuInput.InputText
+                  value={value}
+                  onChangeText={onChange}
+                  placeholder={'Repeat'}
+                  leftIcon={<LockIcon stroke={color.Dark[50]} />}
+                  password
+                  onFocus={() => {
+                    handleFocusInput('confirmPassword');
+                  }}
+                  onBlur={() => {
+                    handleFocusInput(null);
+                  }}
+                  isError={errors?.confirmPassword ? true : false}
+                  errorMsg={errors?.confirmPassword?.message}
+                  isFocus={focusInput === 'confirmPassword'}
+                />
+              )}
+            />
+            <Gap height={14} />
+            <Controller
+              name="termsCondition"
+              control={control}
+              render={({field: {onChange, value}}) => (
+                <TermAndConditions
+                  handleOnPress={() => onChange(!value)}
+                  active={value}
+                  errorMsg={errors?.termsCondition?.message}
+                  onTncPress={() =>
+                    handleWebview(
+                      'Terms Conditions',
+                      'https://sunnysideup.io/marketplace/tos',
+                    )
+                  }
+                  onPrivacyPress={() => {
+                    handleWebview(
+                      'Privacy Policy',
+                      'https://sunnysideup.io/marketplace/privacy-policy',
+                    );
+                  }}
+                />
+              )}
+            />
+            <Gap height={20} />
+            <Button
+              label="Submit"
+              textStyles={{fontSize: mvs(14)}}
+              containerStyles={{width: '100%'}}
+              onPress={handleSubmit(handleRegisterUser)}
+            />
+            <Gap height={4} />
+            <Button
+              label="Back"
+              type="border"
+              borderColor="transparent"
+              textStyles={{fontSize: mvs(14), color: color.Pink.linear}}
+              containerStyles={{width: '100%'}}
+              onPress={handleOnPressBack}
+            />
+          </View>
+          <View>
+            <Text style={styles.forgotPassStyle}>
+              Already have an Account?{' '}
+              <Text
+                onPress={() => handleOnPressSignIn()}
+                style={{
+                  fontFamily: font.InterRegular,
+                  fontWeight: '700',
+                  fontSize: mvs(12),
+                }}>
+                Sign In
+              </Text>
             </Text>
-          </Text>
-        </View>
-      </ScrollView>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
       <ModalLoading visible={isLoading} />
-      <KeyboardAvoidingView
-        behavior={
-          Platform.OS === 'ios' ? 'padding' : 'height'
-        }></KeyboardAvoidingView>
     </View>
   );
 };
