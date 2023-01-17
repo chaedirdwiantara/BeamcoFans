58a14a05ec26a1dabbf085a79922b5cfb8390b5d
chaedir dwiantara <chaedirdwiantara@chaedirs-MacBook-Pro.local>
2023-01-17 19:26:49 +0700

    add navigate into detail

--- src/screen/Detail/PostDetail.tsx
+++ src/screen/Detail/PostDetail.tsx
@@ -443,6 +443,10 @@ export const PostDetail: FC<PostDetailProps> = ({route}: PostDetailProps) => {
     setModalSuccessDonate(false);
   };
 
+  const handleToDetail = (id: string) => {
+    navigation.navigate('MusicianProfile', {id});
+  };
+
   return (
     <SafeAreaView style={styles.root}>
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -457,6 +461,7 @@ export const PostDetail: FC<PostDetailProps> = ({route}: PostDetailProps) => {
         <View style={styles.bodyContainer}>
           {dataPostDetail ? (
             <DetailPost
+              toDetailOnPress={() => handleToDetail(data.musician.uuid)}
               musicianName={musicianName}
               musicianId={`@${data.musician.username}`}
               imgUri={data.musician.imageProfileUrl}
