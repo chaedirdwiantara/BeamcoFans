d47dd28773b0bd6486100c798e8edda9b2b3a02b
nekaartajaya_ssu <neka.artajaya@sunnysideup.io>
2023-02-01 11:13:52 +0800

    feat: refactor all search to react query

--- src/hooks/use-search.hook.tsx
+++ src/hooks/use-search.hook.tsx
@@ -7,14 +7,7 @@ import {
   playlistSearch,
   songSearch,
 } from '../api/search.api';
-import {
-  ListDataSearchAlbums,
-  ListDataSearchFans,
-  ListDataSearchMusician,
-  ListDataSearchPlaylist,
-  ListDataSearchSongs,
-  SearchProps,
-} from '../interface/search.interface';
+import {SearchProps} from '../interface/search.interface';
 import {BannerList} from '../interface/banner.interface';
 import {PaginationType} from '../interface/base.interface';
 import {searchEvent} from '../api/event.api';
@@ -22,84 +15,61 @@ import {searchEvent} from '../api/event.api';
 export const useSearchHook = () => {
   const [searchLoading, setSearchLoading] = useState<boolean>(false);
   // const [searchError, setSearchError] = useState<string>('');
-  const [dataSearchFans, setDataSearchFans] = useState<
-    ListDataSearchFans[] | null
-  >(null);
-  const [dataSearchMusicians, setDataSearchMusicians] = useState<
-    ListDataSearchMusician[] | null
-  >(null);
-  const [dataSearchSongs, setDataSearchSongs] = useState<
-    ListDataSearchSongs[] | null
-  >(null);
-  const [dataSearchAlbums, setDataSearchAlbums] = useState<
-    ListDataSearchAlbums[] | null
-  >(null);
-  const [dataSearchPlaylists, setDataSearchPlaylists] = useState<
-    ListDataSearchPlaylist[] | null
-  >(null);
   const [dataPublicBanner, setDataPublicBanner] = useState<BannerList[]>([]);
 
   const getSearchFans = async (props?: SearchProps) => {
-    setSearchLoading(true);
     try {
       const response = await fansSearch(props);
-      setDataSearchFans(response.data);
+      return {
+        data: response?.data,
+      };
     } catch (error) {
       console.log(error);
-      setDataSearchFans(null);
-    } finally {
-      setSearchLoading(false);
     }
   };
 
   const getSearchMusicians = async (props?: SearchProps) => {
-    setSearchLoading(true);
     try {
       const response = await musicianSearch(props);
-      setDataSearchMusicians(response?.data);
+      return {
+        data: response?.data,
+      };
     } catch (error) {
       console.log(error);
-    } finally {
-      setSearchLoading(false);
     }
   };
 
   const getSearchSongs = async (props?: SearchProps) => {
-    setSearchLoading(true);
     try {
       const response = await songSearch(props);
-      setDataSearchSongs(response.data);
+      console.log(response.data);
+      return {
+        data: response?.data,
+      };
     } catch (error) {
       console.log(error);
-      setDataSearchSongs(null);
-    } finally {
-      setSearchLoading(false);
     }
   };
 
   const getSearchAlbums = async (props?: SearchProps) => {
-    setSearchLoading(true);
     try {
       const response = await albumSearch(props);
-      setDataSearchAlbums(response.data);
+      return {
+        data: response?.data,
+      };
     } catch (error) {
       console.log(error);
-      setDataSearchAlbums(null);
-    } finally {
-      setSearchLoading(false);
     }
   };
 
   const getSearchPlaylists = async (props?: SearchProps) => {
-    setSearchLoading(true);
     try {
       const response = await playlistSearch(props);
-      setDataSearchPlaylists(response.data);
+      return {
+        data: response?.data,
+      };
     } catch (error) {
       console.log(error);
-      setDataSearchPlaylists(null);
-    } finally {
-      setSearchLoading(false);
     }
   };
 
@@ -135,6 +105,7 @@ export const useSearchHook = () => {
     setSearchLoading(true);
     try {
       const response = await listBannerPublic(props);
+      console.log({response});
       setDataPublicBanner(response.data);
     } catch (error) {
       console.log(error);
@@ -146,11 +117,6 @@ export const useSearchHook = () => {
 
   return {
     searchLoading,
-    dataSearchFans,
-    dataSearchMusicians,
-    dataSearchSongs,
-    dataSearchAlbums,
-    dataSearchPlaylists,
     dataPublicBanner,
     getSearchFans,
     getSearchMusicians,
