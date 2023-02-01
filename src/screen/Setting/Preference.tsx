5b45a4f5f52bd5c856b69076f9e4e6d4297fa448
nekaartajaya_ssu <neka.artajaya@sunnysideup.io>
2023-02-01 18:02:08 +0800

    feat: ui setting preference

--- src/screen/Setting/Preference.tsx
+++ src/screen/Setting/Preference.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
@@ -6,12 +6,13 @@ import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import Color from '../../theme/Color';
 import {RootStackParams} from '../../navigations';
 import {ArrowLeftIcon} from '../../assets/icon';
-import {Dropdown, TopNavigation} from '../../components';
-import {dataLanguage} from '../../data/Settings/language';
+import {Button, Dropdown, Gap, TopNavigation} from '../../components';
+import {dataGenres} from '../../data/Settings/genre';
+import {dataMoods} from '../../data/Settings/mood';
 import {heightPercentage, widthPercentage} from '../../utils';
+import {mvs} from 'react-native-size-matters';
 
 export const PreferenceSettingScreen: React.FC = () => {
-  const [, setLanguage] = useState('');
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParams>>();
 
@@ -28,12 +29,29 @@ export const PreferenceSettingScreen: React.FC = () => {
         leftIconAction={onPressGoBack}
         containerStyles={{marginBottom: heightPercentage(15)}}
       />
-      <Dropdown.Input
-        data={dataLanguage}
-        placeHolder={'Search Language'}
-        dropdownLabel={'Language'}
-        textTyped={(newText: string) => setLanguage(newText)}
+      <Dropdown.Multi
+        data={dataGenres}
+        placeHolder={'Select Genre'}
+        dropdownLabel={'Genre'}
+        textTyped={(_newText: string) => null}
         containerStyles={{marginTop: heightPercentage(15)}}
+        initialValue={[]}
+      />
+      <Gap height={10} />
+      <Dropdown.Multi
+        data={dataMoods}
+        placeHolder={'Select Mood'}
+        dropdownLabel={'Mood'}
+        textTyped={(_newText: string) => null}
+        containerStyles={{marginTop: heightPercentage(15)}}
+        initialValue={[]}
+      />
+      <Gap height={40} />
+      <Button
+        label="Save"
+        textStyles={{fontSize: mvs(14)}}
+        containerStyles={{width: '100%'}}
+        onPress={() => null}
       />
     </View>
   );
