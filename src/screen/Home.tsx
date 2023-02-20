a2ba57ff93ed466a0defbe68e1ba1b82f139f756
kriptonhaz <kriptonhaz@gmail.com>
2023-02-20 20:16:47 +0700

    wiring get credit

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -54,6 +54,7 @@ import {ModalPlayMusic} from '../components/molecule/Modal/ModalPlayMusic';
 import {heightPercentage, widthPercentage, widthResponsive} from '../utils';
 import {defaultBanner} from '../data/home';
 import {useNotificationHook} from '../hooks/use-notification.hook';
+import {useCreditHook} from '../hooks/use-credit.hook';
 
 type OnScrollEventHandler = (
   event: NativeSyntheticEvent<NativeScrollEvent>,
@@ -92,6 +93,7 @@ export const HomeScreen: React.FC<HomeProps> = ({route}: HomeProps) => {
     getListDataBannerPublic,
   } = useSearchHook();
   const {counter, getCountNotification} = useNotificationHook();
+  const {creditCount} = useCreditHook();
 
   const isLogin = storage.getBoolean('isLogin');
   const isFocused = useIsFocused();
@@ -244,7 +246,7 @@ export const HomeScreen: React.FC<HomeProps> = ({route}: HomeProps) => {
         rightIconAction={onPressNotif}
         maxLengthTitle={20}
         itemStrokeColor={Color.Pink[100]}
-        points={isLogin ? 100000 : 0}
+        points={isLogin ? creditCount : 0}
         containerStyles={{paddingHorizontal: widthResponsive(24)}}
         onPressCoin={onPressCoin}
         guest={!isLogin}
