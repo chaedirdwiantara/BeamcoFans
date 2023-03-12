a8b9504398721c3591d64947be0feccc3482b7e3
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2023-03-13 02:29:03 +0700

    SM-248: handle helps press

--- src/components/molecule/SettingContent/SettingContent.tsx
+++ src/components/molecule/SettingContent/SettingContent.tsx
@@ -53,7 +53,11 @@ export const SettingContent: React.FC<SettingProps> = ({
         modalReport: true,
         modalConfirm: false,
       });
-    } else if (val === 'Terms and Conditions' || val === 'Privacy Policy') {
+    } else if (
+      val === 'Terms and Conditions' ||
+      val === 'Privacy Policy' ||
+      val === 'Helps'
+    ) {
       const path = val === t('Setting.TnC.Title') ? 'tos' : 'privacy-policy';
       handleWebview(
         val === 'Terms and Conditions'
