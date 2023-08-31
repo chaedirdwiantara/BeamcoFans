c8f635c8f034f18535dae6bbe7026bf5c0239e89
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2023-08-31 16:48:09 +0700

    BEAM-827: Add new menu screen account

--- src/components/molecule/SettingContent/SettingContent.tsx
+++ src/components/molecule/SettingContent/SettingContent.tsx
@@ -170,7 +170,6 @@ export const SettingContent: React.FC<SettingProps> = ({
           <MenuText.RightIcon
             key={i}
             text={t(val.text) || ''}
-            textStyles={{fontSize: mvs(14)}}
             containerStyles={{marginTop: heightPercentage(15)}}
             onPress={() => onPress(val.value)}
           />
