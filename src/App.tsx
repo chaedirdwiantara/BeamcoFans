5d91921a689269eac4edaa84f28b372b032d0a8c
kriptonhaz <khamzah.muhammad@sunnysideup.io>
2023-09-05 12:04:24 +0700

    add sentry

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ import {RootStackScreen} from './navigations';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {CrashLog, CrashRecord} from './service/crashReport';
 
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://0eb38ae5f289dd77f6943db24ce7b549@o4505644342050816.ingest.sentry.io/4505820192702464',
+});
+
 const App = () => {
   const queryClient = new QueryClient();
 
@@ -30,4 +36,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.wrap(App);
