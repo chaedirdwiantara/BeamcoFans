929d8f6714f31ad837c796009ff579cd0d444a0d
kriptonhaz <khamzah.muhammad@sunnysideup.io>
2023-11-20 16:17:35 +0700

    update ios native files

--- ios/beamcoTests/beamcoTests.m
+++ ios/beamcoTests/beamcoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SsuUserTests : XCTestCase
+@interface beamcoTests : XCTestCase
 
 @end
 
-@implementation SsuUserTests
+@implementation beamcoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
