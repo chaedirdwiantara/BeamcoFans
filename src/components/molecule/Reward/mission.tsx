47376dd90c909db4636d2f95ba452c927f80868c
chaedir dwiantara <chaedirdwiantara@chaedirs-MacBook-Pro.local>
2023-12-01 20:37:36 +0700

    fix text in progressbar

--- src/components/molecule/Reward/mission.tsx
+++ src/components/molecule/Reward/mission.tsx
@@ -82,36 +82,41 @@ const Mission: React.FC<MissionProps> = ({data, onClaim, onGo}) => {
           </View>
         </View>
         <Gap height={12} />
-        <Progress.Bar
-          progress={
-            data.taskType === 'based-reward' ? progressRepeatable : progressBar
-          }
-          width={null}
-          height={widthResponsive(11)}
-          borderWidth={0}
-          color={color.Pink[200]}
-          unfilledColor={color.Dark[300]}
-          borderRadius={4}
-          animated={false}
-          // animationType={'timing'}
-        />
-        {!dataProgress?.isClaimed ? (
-          <View style={styles.progressContainer}>
-            <Text style={styles.progressTxt}>
-              {data.taskType === 'based-reward'
-                ? progressTextRepeatable
-                : progressText}
-            </Text>
-          </View>
-        ) : (
-          <View style={styles.progressContainer}>
-            <CheckCircle2Icon width={10} height={10} />
-            <Gap width={4} />
-            <Text style={styles.progressTxt}>
-              {t('Rewards.MissionTab.BtnCompleted')}
-            </Text>
-          </View>
-        )}
+        <View style={styles.prgContainer}>
+          <Progress.Bar
+            progress={
+              data.taskType === 'based-reward'
+                ? progressRepeatable
+                : progressBar
+            }
+            width={null}
+            height={widthResponsive(11)}
+            borderWidth={0}
+            color={color.Pink[200]}
+            unfilledColor={color.Dark[300]}
+            borderRadius={4}
+            animated={false}
+            style={{width: '100%'}}
+            // animationType={'timing'}
+          />
+          {!dataProgress?.isClaimed ? (
+            <View style={styles.progressContainer}>
+              <Text style={styles.progressTxt}>
+                {data.taskType === 'based-reward'
+                  ? progressTextRepeatable
+                  : progressText}
+              </Text>
+            </View>
+          ) : (
+            <View style={styles.progressContainer}>
+              <CheckCircle2Icon width={10} height={10} />
+              <Gap width={4} />
+              <Text style={styles.progressTxt}>
+                {t('Rewards.MissionTab.BtnCompleted')}
+              </Text>
+            </View>
+          )}
+        </View>
       </View>
 
       {!dataProgress?.isClaimed && (
@@ -151,12 +156,13 @@ const styles = StyleSheet.create({
     position: 'relative',
     justifyContent: 'space-between',
   },
+  prgContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
   progressContainer: {
     position: 'absolute',
-    top: widthResponsive(28),
-    left: 0,
-    right: 0,
-    bottom: 0,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
