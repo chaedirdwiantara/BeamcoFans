f4a4fd23e8b82687e69542a877d95b147557d1a9
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2024-01-19 15:17:34 +0700

    BEAM-1752: Update handle screen main tab when user login as guest

--- src/hooks/use-badge.hook.tsx
+++ src/hooks/use-badge.hook.tsx
@@ -3,10 +3,11 @@ import {checkBadgeLevel} from '../api/badge.api';
 import {BadgePropsType} from '../interface/badge.interface';
 
 export const useBadgeHook = () => {
-  const useCheckBadge = (props: BadgePropsType) =>
+  const useCheckBadge = (props: BadgePropsType, enabled?: boolean) =>
     useQuery({
       queryKey: ['badge', props],
       queryFn: () => checkBadgeLevel(props),
+      enabled,
     });
 
   return {
